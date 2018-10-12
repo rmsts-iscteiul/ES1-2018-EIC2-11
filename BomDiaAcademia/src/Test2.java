ee55c5650893560dc1ce4a41a3ed7e31d80f6119
ragsa1-iscteiul <ragsa1@iscte-iul.pt>
2018-10-12 18:41:12 +0100

    This is the exercise done by ragsa1 (4.B)

---
+++ BomDiaAcademia/src/Test2.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Done by Ragsa1");
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(1);
+		list.add(2);
+	}
+}
