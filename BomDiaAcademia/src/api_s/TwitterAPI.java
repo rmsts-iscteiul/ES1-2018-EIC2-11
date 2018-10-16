1625276ae4e87e796d05789b91aa460d33b54ca7
ragsa1-iscteiul <ragsa1@iscte-iul.pt>
2018-10-16 11:40:50 +0100

    It shows the tweets from @ISCTEIUL

--- BomDiaAcademia/src/api_s/TwitterAPI.java
+++ BomDiaAcademia/src/api_s/TwitterAPI.java
@@ -7,31 +7,31 @@ import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-public final class TwitterAPI  {
+public class TwitterAPI {
 	public static void main(String[] args) {
 
-        try {
-        	ConfigurationBuilder cb = new ConfigurationBuilder();
-        	cb.setDebugEnabled(true)
-        	  .setOAuthConsumerKey("GahzkZi50ZuasuqqkRqckC2ln")
-        	  .setOAuthConsumerSecret("Rr3Q7ivhL3HiEKVoP3d2aLwNbh6ez5rTdEy3MtNRd6yeC5vNVV")
-        	  .setOAuthAccessToken("3362933517-swAMAbDiMufcuyxHUEpkSYUJ3JY9ANIrqT5yirP")
-        	  .setOAuthAccessTokenSecret("onDIulYjcQCvb3rVk6N3cYp0DxytW0ew86fM2Kyp8JQOj");
-        	TwitterFactory tf = new TwitterFactory(cb.build());
-        	Twitter twitter = tf.getInstance();      		
-            List<Status> statuses = twitter.getHomeTimeline();
-            System.out.println("------------------------\n Showing home timeline \n------------------------");
-    		int counter=0;
-    		int counterTotal = 0;
-            for (Status status : statuses) {
+		try {
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			cb.setDebugEnabled(true).setOAuthConsumerKey("GahzkZi50ZuasuqqkRqckC2ln")
+					.setOAuthConsumerSecret("Rr3Q7ivhL3HiEKVoP3d2aLwNbh6ez5rTdEy3MtNRd6yeC5vNVV")
+					.setOAuthAccessToken("3362933517-swAMAbDiMufcuyxHUEpkSYUJ3JY9ANIrqT5yirP")
+					.setOAuthAccessTokenSecret("onDIulYjcQCvb3rVk6N3cYp0DxytW0ew86fM2Kyp8JQOj");
+			TwitterFactory tf = new TwitterFactory(cb.build());
+			Twitter twitter = tf.getInstance();
+			List<Status> statuses = twitter.getUserTimeline("@ISCTEIUL");
+			System.out.println("------------------------\n Showing home timeline \n------------------------");
+			int counter = 0;
+			int counterTotal = 0;
+			for (Status status : statuses) {
 				if (status.getUser().getName() != null ) {
-					System.out.println(status.getUser().getName() + ":" + status.getText());
+					System.out.println(status.getUser().getName() + ": " + status.getText());
 					counter++;
 				}
 				counterTotal++;
-            }
-    		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
-        } catch (Exception e) { System.out.println(e.getMessage()); }
-     }
+			}
+			System.out.println("-------------\nNº of Results: " + counter + "/" + counterTotal);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
-    
