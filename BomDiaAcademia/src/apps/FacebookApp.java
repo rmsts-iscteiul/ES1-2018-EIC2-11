5d763ea64573e1f8f625880caa5f18fdb5985fe3
ljcra-iscteiul <ljcra@iscte-iul.pt>
2018-10-22 20:23:40 +0100

    Just added facebook JavaDoc

--- BomDiaAcademia/src/apps/FacebookApp.java
+++ BomDiaAcademia/src/apps/FacebookApp.java
@@ -10,11 +10,26 @@ import com.restfb.FacebookClient;
 import com.restfb.types.Post;
 import com.restfb.types.User;
 
+/**
+ * This is the FacebookApp that uses restfb library.
+ * @author ES1-2018-EIC2-11
+ */
 public class FacebookApp {
 
+	/**
+	 * fbClient is to initiate communication with facebook trough accessToken
+	 */
 	private static FacebookClient fbClient;
+
+	/**
+	 * Object fetched from facebook
+	 */
 	private static User me;
 
+	/**
+	 * Initilize app(fbClient) with given acessToken, and fetched "me" object from
+	 * facebook.
+	 */
 	@SuppressWarnings("deprecation")
 	public FacebookApp() {
 		String accessToken = "EAAQSXbaizwwBAK2M96ZBGFaYFw7Ez8yaZAM6Ap9ZB1Lrou1kKi2PoGE1E96CUNE4pVokBfhAgZCIucDlb7r9J45O9mCkZAxZAZA9GIvWE27ZB8wtS6CUhIwjcoPVNGaChu87YH04taeksWTcmHuBKHI2M6oZAOR6EdYWTn1ka791HFwZDZD";
@@ -22,6 +37,11 @@ public class FacebookApp {
 
 		me = fbClient.fetchObject("me", User.class);
 	}
+	
+	/**
+	 * Returns all non-null message posts from user feed
+	 * @return List where all non-null message posts from user feed are included.
+	 */
 
 	public List<Post> getTimeline() {
 		List<Post> posts = new LinkedList<>();
@@ -35,11 +55,17 @@ public class FacebookApp {
 				}
 			}
 
-		} catch (Exception e) { // When Facebook service or network is unavailable
+		} catch (Exception e) { 
 			System.out.println(e.getMessage());
 		}
 		return posts;
 	}
+	
+	/**
+	 * Returns all non-null message posts from user feed, based on a filter
+	 * @return List where all non-null message posts based on a filter are included, from user feed.
+	 * @param filter chosen by user to get specific posts.
+	 */
 
 	public List<Post> getTimeline(String filter) {
 		List<Post> posts = new LinkedList<>();
@@ -53,12 +79,16 @@ public class FacebookApp {
 				}
 			}
 
-		} catch (Exception e) { // When Facebook service or network is unavailable
+		} catch (Exception e) { 
 			System.out.println(e.getMessage());
 		}
 		return posts;
 	}
 
+	/**
+	 * return user's name
+	 * @return String that equals user's name
+	 */
 	public String getUser() {
 		return me.getName();
 	}
