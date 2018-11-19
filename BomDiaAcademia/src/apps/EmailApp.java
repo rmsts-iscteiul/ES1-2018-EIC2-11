762ca10efbdb5b8b53370dc47dd098b457d0161e
msjrr-iscteiul <msjrr@iscte-iul.pt>
2018-11-19 23:28:45 +0000

    Email App
    Email sending function:
    simple email sending
    email with attachments sending

--- BomDiaAcademia/src/apps/EmailApp.java
+++ BomDiaAcademia/src/apps/EmailApp.java
@@ -2,97 +2,212 @@ package apps;
 
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.NoSuchProviderException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.Transport;
 
 public class EmailApp {
 
-	public class test {
-		private String filterS;
+	private String filterS;
+	private String user;
+	private String password;
 
-		public void check(String host, String port, String storeType, String user, String password) {
-			try {
+	private void check(String user, String password) {
+		this.user = user;
+		this.password = password;
+		try {
 
-				// create properties field
-				Properties properties = new Properties();
+			// create properties field
+			Properties properties = new Properties();
 
-				properties.put("mail.pop3.host", host);
-				properties.put("mail.pop3.port", port);
-				properties.put("mail.pop3.starttls.enable", "true");
-				Session emailSession = Session.getDefaultInstance(properties);
+			properties.put("mail.pop3.host", "pop-mail.outlook.com");
+			properties.put("mail.pop3.port", "995");
+			properties.put("mail.pop3.starttls.enable", "true");
+			Session emailSession = Session.getDefaultInstance(properties);
 
-				// create the POP3 store object and connect with the pop server
-				Store store = emailSession.getStore("pop3s");
+			// create the POP3 store object and connect with the pop server
+			Store store = emailSession.getStore("pop3s");
 
-				store.connect(host, user, password);
+			store.connect("pop-mail.outlook.com", user, password);
 
-				// create the folder object and open it
-				Folder emailFolder = store.getFolder("INBOX");
-				emailFolder.open(Folder.READ_ONLY);
+			// create the folder object and open it
+			Folder emailFolder = store.getFolder("INBOX");
+			emailFolder.open(Folder.READ_ONLY);
 
-				// retrieve the messages from the folder in an array and print it
-				Message[] messages = emailFolder.getMessages();
-				System.out.println("messages.length---" + messages.length);
-				if (this.filterS != null) {
-					System.out.println("PIMBA");
-					for (int i = 0, n = messages.length; i < n; i++) {
-						Message message = messages[i];
-						String aux = "" + message.getContent();
-						if (aux.toLowerCase().indexOf(filterS.toLowerCase()) != -1) {
-							System.out.println("---------------------------------");
-							System.out.println("Email Number " + (i + 1));
-							System.out.println("Subject: " + message.getSubject());
-							System.out.println("From: " + message.getFrom()[0]);
-							// System.out.println("Text: " + message.getContent().toString());
-						} else {
-							System.out.println("No results match the searching criteria");
-						}
-					}
-				} else {
-					System.out.println("WOWOWOW");
-					for (int i = messages.length - 1, n = messages.length - 21; i > n; i--) {
-						Message message = messages[i];
+			// retrieve the messages from the folder in an array and print it
+			Message[] messages = emailFolder.getMessages();
+			System.out.println("messages.length---" + messages.length);
+			if (this.filterS != null) {
+
+				for (int i = 0, n = messages.length; i < n; i++) {
+					Message message = messages[i];
+					String aux = "" + message.getContent();
+					if (aux.toLowerCase().indexOf(filterS.toLowerCase()) != -1) {
 						System.out.println("---------------------------------");
 						System.out.println("Email Number " + (i + 1));
 						System.out.println("Subject: " + message.getSubject());
 						System.out.println("From: " + message.getFrom()[0]);
 						// System.out.println("Text: " + message.getContent().toString());
-
+					} else {
+						System.out.println("No results match the searching criteria");
 					}
 				}
+			} else {
 
-				// close the store and folder objects
-				emailFolder.close(false);
-				store.close();
+				for (int i = messages.length - 1, n = messages.length - 21; i > n; i--) {
+					Message message = messages[i];
+					System.out.println("---------------------------------");
+					System.out.println("Email Number " + (i + 1));
+					System.out.println("Subject: " + message.getSubject());
+					System.out.println("From: " + message.getFrom()[0]);
+					// System.out.println("Text: " + message.getContent().toString());
 
-			} catch (NoSuchProviderException e) {
-				e.printStackTrace();
-			} catch (MessagingException e) {
-				e.printStackTrace();
-			} catch (Exception e) {
-				e.printStackTrace();
+				}
 			}
-		}
 
-		public void filter(String filterS) {
-			this.filterS = filterS;
-		}
+			// close the store and folder objects
+			emailFolder.close(false);
+			store.close();
 
-//		public static void main(String[] args) {
-//			test email = new test();
-//			String host = "pop-mail.outlook.com";// change accordingly
-//			String port = "995"; // change accordingly
-//			String mailStoreType = "pop3";
-//			String username = "msjrr@iscte-iul.pt";// change accordingly
-//			String password = // change accordingly
-//			email.filter("estudantes");
-//			email.check(host, port, mailStoreType, username, password);
-//
-//		}
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sendEmail(String to, String text) {
+		String host = "smtp-mail.outlook.com";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "587");
+
+		// Get the Session object.
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password);
+			}
+		});
+		try {
+			// Create a default MimeMessage object.
+			Message message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(user));
+
+			// Set To: header field of the header.
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+			// Set Subject: header field
+			message.setSubject("Testing Subject");
+
+			// Now set the actual message
+			message.setText(text);
+
+			// Send message
+			Transport.send(message);
+
+			System.out.println("Sent message successfully....");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 
 	}
+	
+	private void sendEmailWithAttachment(String to, String text) {
+		String host = "smtp-mail.outlook.com";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "587");
+
+		// Get the Session object.
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password);
+			}
+		});
+		 try {
+	         // Create a default MimeMessage object.
+	         Message message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(user));
+
+	         // Set To: header field of the header.
+	         message.setRecipients(Message.RecipientType.TO,
+	            InternetAddress.parse(to));
+
+	         // Set Subject: header field
+	         message.setSubject("Testing Subject");
+
+	         // Create the message part
+	         BodyPart messageBodyPart = new MimeBodyPart();
+
+	         // Now set the actual message
+	         messageBodyPart.setText("This is message body");
+
+	         // Create a multipar message
+	         Multipart multipart = new MimeMultipart();
+
+	         // Set text message part
+	         multipart.addBodyPart(messageBodyPart);
+
+	         // Part two is attachment
+	         messageBodyPart = new MimeBodyPart();
+	         String filename = "/home/manisha/file.txt";
+	         DataSource source = new FileDataSource(filename);
+	         messageBodyPart.setDataHandler(new DataHandler(source));
+	         messageBodyPart.setFileName(filename);
+	         multipart.addBodyPart(messageBodyPart);
+
+	         // Send the complete message parts
+	         message.setContent(multipart);
+
+	         // Send message
+	         Transport.send(message);
+
+	         System.out.println("Sent message successfully....");
+	  
+	      } catch (MessagingException e) {
+	         throw new RuntimeException(e);
+	      }
+	}
+
+	public void filter(String filterS) {
+		this.filterS = filterS;
+	}
+
+//	public static void main(String[] args) {
+//		EmailApp email = new EmailApp();
+//
+//		String username = "msjrr@iscte-iul.pt";// change accordingly
+//		String password = "*******";// change accordingly
+//		//email.check(username, password);
+//		email.sendEmail("matiasfrazaocorreia@gmail.com", "Bom dia academia teste!");
+//	
+//	 }
+
 }
