6dda01bb3a27dff3d0a936c4b62dbe2b8092d7c4
ragsa1-iscteiul <ragsa1@iscte-iul.pt>
2018-12-01 22:49:48 +0000

    Retweet features and replyTo feature

--- BomDiaAcademia/src/apps/TwitterApp.java
+++ BomDiaAcademia/src/apps/TwitterApp.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -23,7 +24,9 @@ public class TwitterApp {
 	 * value of "ISCTEIUL" (default value).
 	 */
 	private String user = "ISCTEIUL";
-	
+
+	private final String WATERMARK = "\n" + "\n" + "-Tweet sent through BomDiaAcademia Application <3";
+
 	/**
 	 * twitter refers to the twitter object from the twitterfactory that uses an
 	 * ConsumerKey and a Access Token.
@@ -31,15 +34,16 @@ public class TwitterApp {
 	private Twitter twitter;
 
 	private String owner = "";
-	
+
 	/**
 	 * Deprecated list of a users tweets
 	 */
 	private List<Status> statuses;
 
 	/**
-	 * Constructor of the TwitterApp. It uses an Access token and an Consumer key to
-	 * build the twitter factory which uses to build the twitter instance.
+	 * Constructor of the TwitterApp. It uses an Access token and an Consumer
+	 * key to build the twitter factory which uses to build the twitter
+	 * instance.
 	 */
 	public TwitterApp() {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -52,45 +56,52 @@ public class TwitterApp {
 		try {
 			owner = twitter.getScreenName();
 		} catch (IllegalStateException | TwitterException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 	/**
-	 * timeline of the user that is logged-in
-	 * @return the timeline of the user that is logged-in
+	 * 
+	 * @return timeline of the user that is logged-in
 	 */
-	public List<Status> homepage(){
+	public List<Status> homepage() {
 		return getTimeline(owner);
 	}
-	
+
 	/**
 	 * Gets the most recent 20 posts from a users timeline.
 	 * 
-	 * @param user(String) Owns the timeline which is being displayed.
+	 * @param user(String)
+	 *            Owns the timeline which is being displayed.
 	 * @return List(Status) - Contains the last 20 tweets of the user.
 	 */
 	@SuppressWarnings("finally")
-	public List<Status> getTimeline(String user) { // The argument need to be the @ of the user. DON'T
+	public List<Status> getTimeline(String user) { // The argument need to be
+													// the @ of the user. DON'T
 													// WRITE THE '@'
 		try {
 			statuses = twitter.getUserTimeline(user);
 			this.user = user;
 
-		} catch (TwitterException e) { // when Twitter service or network is unavailable this will return the
+		} catch (TwitterException e) { // when Twitter service or network is
+										// unavailable this will return the
 										// un-updated LIST
 			System.out.println("CHECK NETWORK CONNECTION");
-		}finally {
+		} finally {
 			return statuses;
 		}
 	}
 
 	/**
-	 * Gets the timeline of the user filtered with the 2º argument of this method
-	 * (filter)
+	 * Gets the timeline of the user filtered with the 2º argument of this
+	 * method (filter)
 	 * 
-	 * @param user(String) Owns the timeline which is being displayed.
-	 * @param filter(String) Chosen by the User. Tweets of the user are filtered
-	 *        using this argument.
+	 * @param user(String)
+	 *            Owns the timeline which is being displayed.
+	 * @param filter(String)
+	 *            Chosen by the User. Tweets of the user are filtered using this
+	 *            argument.
 	 * @return List(Status) - Contains the last 20 tweets based on the filter(2º
 	 *         argument) of the user.
 	 */
@@ -100,33 +111,84 @@ public class TwitterApp {
 		try {
 			statuses = twitter.getUserTimeline(user);
 			this.user = user;
-			
-		} catch (TwitterException e) { // when Twitter service or network is unavailable
+
+		} catch (TwitterException e) { // when Twitter service or network is
+										// unavailable
 			System.out.println("CHECK NETWORK CONNECTION");
-		}finally{
+		} finally {
 			for (Status status : statuses) {
 				if (status.getText().contains(filter)) {
 					list.add(status);
 				}
 			}
 			return list;
-			
+
 		}
 	}
 
 	/**
-	 * Publishes a tweet on the logged users timeline and redirects its user to his homepage
-	 * @param text - Tweets text
+	 * Publishes a tweet on the logged users timeline and redirects its user to
+	 * his homepage
+	 * 
+	 * @param text - tweets this text
+	 * @return returns the owners timeline
 	 */
-	public List<Status> tweet(Status text){
+	public List<Status> tweet(String text) {
 		try {
-			twitter.updateStatus(text + "\n" + "\n" + "-Tweet sent through BomDiaAcademia Application <3");
+			StatusUpdate newStatus = new StatusUpdate(text);
+			twitter.updateStatus(newStatus.getStatus() + WATERMARK);
 			return homepage();
 		} catch (TwitterException e) {
 			System.out.println("CHECK CONNECTION - Tweet was possibly not published.");
 			return statuses;
 		}
 	}
+
+	/**
+	 * Replies to a status (comment)
+	 * @param text - the text you want to comment
+	 * @param postID - the id of the status you want to comment
+	 */
+	public void replyTo(String text, long postID) { // Comment as a reply to @user.
+		try {
+			StatusUpdate newStatus = new StatusUpdate("@" + user + "\n" + text);
+			newStatus.setInReplyToStatusId(postID);
+			twitter.updateStatus(newStatus + WATERMARK);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	/**
+	 * Retweets a certain status
+	 * @param postID - the id of the status you want to retweet
+	 */
+	public void retweet(long postID) { //Share a post
+		try {
+			twitter.retweetStatus(postID);
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Retweets a certain status with a message (comment and share basically)
+	 * @param postID - the id of the status you want to retweet
+	 * @param text - the text you want to comment
+	 */
+	public void retweet(long postID, String text){ //Basically It's to comment and share a post
+		try{
+			StatusUpdate newStatus = new StatusUpdate(text);
+			newStatus.setAttachmentUrl("https://twitter.com/" + user + "/status/" + postID);
+			twitter.updateStatus(newStatus + WATERMARK);
+		}catch (TwitterException e){
+			e.printStackTrace();
+		}
+	}
+
 	
 	/**
 	 * Returns the user that is being displayed.
@@ -136,21 +198,33 @@ public class TwitterApp {
 	public String getUser() {
 		return user;
 	}
-	
+
 	/**
 	 * Changes users page
+	 * 
 	 * @param user
 	 */
 	public void setUser(String user) {
 		this.user = user;
 	}
-	
+
 	/**
 	 * User that is logged-in
+	 * 
 	 * @return the "@"name of the user that is logged-in
 	 */
-	public String getOwner(){
+	public String getOwner() {
 		return owner;
 	}
+
+	//BEFORE JUNIT
+//	public static void main(String[] args) {
+//		TwitterApp t = new TwitterApp();
+//		System.out.println(t.getTimeline("Slbenfica").get(0).getText());
+//		System.out.println(t.getTimeline("Slbenfica").get(0).getId());
+//		long id = (long) t.getTimeline("Slbenfica").get(0).getId();
+//		//t.replyTo("Random !", id);
+//		t.retweet(id, "something something");
+//	}
 
 }
