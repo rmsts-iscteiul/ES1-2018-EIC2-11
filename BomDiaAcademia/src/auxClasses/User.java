2a25e269cbb77c9f1ce699e89d412b1e5ebc5cd1
msjrr-iscteiul <msjrr@iscte-iul.pt>
2018-12-06 23:01:14 +0000

    Update User configs method implemented

--- BomDiaAcademia/src/auxClasses/User.java
+++ BomDiaAcademia/src/auxClasses/User.java
@@ -17,6 +17,7 @@ import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -25,92 +26,184 @@ public class User {
 	private String fn, ln, pw;
 	private Encryptation encrypt = new Encryptation();
 	private String salt = "GPIHqhlAdTKLG2EDX8mq3S3EZFuoEM";
-	
-	public User(String fn, String ln, String pw) {
+	private String fbToken;
+	private String twToken;
+	private String emUsr;
+	private String emPwd;
+	private String darkTheme;
+
+	/**
+	 * @param fn
+	 * @param ln
+	 * @param pw
+	 * 
+	 *            When a user registers in the app the follwing @param are set as
+	 *            null
+	 * @param fbToken
+	 * @param twToken
+	 * @param emUsr
+	 * 
+	 *            When a user registers in the app the following @param is set as
+	 *            '0'
+	 * @param darkTheme
+	 * @param emPwd
+	 *            (This one is due to encryptation requirements)
+	 */
+
+	public User(String fn, String ln, String pw, String fbToken, String twToken, String emUsr, String emPwd,
+			String darkTheme) {
 		this.fn = fn;
 		this.ln = ln;
 		this.pw = pw;
+		this.fbToken = fbToken;
+		this.twToken = twToken;
+		this.emUsr = emUsr;
+		this.emPwd = emPwd;
+		this.darkTheme = darkTheme;
 	}
 
+	public String getFbToken() {
+		return fbToken;
+	}
+
+	public String getTwToken() {
+		return twToken;
+	}
+
+	public String getEmUsr() {
+		return emUsr;
+	}
+
+	public String getEmPwd() {
+		return emPwd;
+	}
+
+	public String getDarkTheme() {
+		return darkTheme;
+	}
 
 	public String getFn() {
 		return fn;
 	}
 
-
 	public String getLn() {
 		return ln;
 	}
 
-
 	public String getPw() {
 		return pw;
 	}
-	
-	public void saveNewUser(User usr) {
-		try {	
-	         File inputFile = new File("src\\resources\\files\\credentials.xml");
-	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	         Document doc = dBuilder.parse(inputFile);
-	         doc.getDocumentElement().normalize();         	      
-	         XPathFactory xpathFactory = XPathFactory.newInstance();
-	         XPath xpath = xpathFactory.newXPath();
-	         XPathExpression expr = xpath.compile("/XML/User/@*");
-	         NodeList nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-	         	        	         
-	             
-	         if(!isUsrRegistered(nl, usr)) {
-	        	 Element newElement1 = doc.createElement("User");
-		         newElement1.setAttribute("fn", usr.getFn());
-		         newElement1.setAttribute("ln", usr.getLn());
-		         newElement1.setAttribute("pw", encrypt.generateSecurePassword(usr.getPw(), salt));
-		         Node n = doc.getDocumentElement();
-		         n.appendChild(newElement1); 
-	         }
-	       
-	         Transformer transformer = TransformerFactory.newInstance().newTransformer();
-	         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-	         StreamResult result = new StreamResult(new FileOutputStream("src\\resources\\files\\credentials.xml"));
-	         DOMSource source = new DOMSource(doc);
-	         transformer.transform(source, result);
-	      } catch (Exception e) { e.printStackTrace(); }
-	}
-	
-	private boolean isUsrRegistered(NodeList nl, User usr) {
-		for (int i = 0; i < nl.getLength(); i++) {
-       	 	if(nl.item(i).getFirstChild().getNodeValue().equals(usr.getFn()) && nl.item(i+1).getFirstChild().getNodeValue().equals(usr.getLn())) {
-       	 		return true;
-       	 	}
-        }
-		return false;
-	}
-	
-	protected boolean isUsrRegistered(User usr) {
-		
+
+	private void saveNewUser(User usr) {
 		try {
 			File inputFile = new File("src\\resources\\files\\credentials.xml");
-	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	         Document doc = dBuilder.parse(inputFile);
-	         doc.getDocumentElement().normalize();         
-	         XPathFactory xpathFactory = XPathFactory.newInstance();
-	         XPath xpath = xpathFactory.newXPath();
-	         XPathExpression expr = xpath.compile("/XML/User/@*");
-	         NodeList nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-	         for (int i = 0; i < nl.getLength(); i++) {
-	        	 	if(nl.item(i).getFirstChild().getNodeValue().equals(usr.getFn()) && nl.item(i+1).getFirstChild().getNodeValue().equals(usr.getLn()) && encrypt.verifyUserPassword(usr.getPw(),nl.item(i+2).getFirstChild().getNodeValue() , salt)) {
-	        	 		return true;
-	        	 	}
-	         }
-		}catch (Exception e) { e.printStackTrace(); }		
-		return false;
-		
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			XPathFactory xpathFactory = XPathFactory.newInstance();
+			XPath xpath = xpathFactory.newXPath();
+			XPathExpression expr = xpath.compile("/XML/User/@*");
+			NodeList nl = doc.getElementsByTagName("User");
+			
+			if (!isUsrRegistered(nl, usr)) {
+				Element newElement1 = doc.createElement("User");
+				newElement1.setAttribute("fn", usr.getFn());
+				newElement1.setAttribute("ln", usr.getLn());
+				newElement1.setAttribute("pw", encrypt.generateSecurePassword(usr.getPw(), salt));
+				newElement1.setAttribute("darkThem", "" + usr.getDarkTheme());
+				newElement1.setAttribute("fbToken", usr.getFbToken());
+				newElement1.setAttribute("twToken", usr.getTwToken());
+				newElement1.setAttribute("emUsr", usr.getEmUsr());
+				newElement1.setAttribute("emPwd", encrypt.generateSecurePassword(usr.getEmPwd(), salt));
+				Node n = doc.getDocumentElement();
+				n.appendChild(newElement1);
+			}
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			StreamResult result = new StreamResult(new FileOutputStream("src\\resources\\files\\credentials.xml"));
+			DOMSource source = new DOMSource(doc);
+			transformer.transform(source, result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-//	public static void main(String[] args) {
-//		User gay = new User("ma","ti","as");
-//		gay.saveNewUser(gay);
-//		System.out.println(gay.isUsrRegistered(gay));
-//	}
+
+	private boolean isUsrRegistered(NodeList nl, User usr) {
+		for (int i = 0; i < nl.getLength(); i++) {
+			NamedNodeMap children = nl.item(i).getAttributes();
+			if(children.getNamedItem("fn").getNodeValue().equals(usr.getFn()) && children.getNamedItem("ln").getNodeValue().equals(usr.getLn())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+
+	private void updateUsrInfo(User usr) {
+		try {
+			File inputFile = new File("src\\resources\\files\\credentials.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			XPathFactory xpathFactory = XPathFactory.newInstance();
+			XPath xpath = xpathFactory.newXPath();
+			XPathExpression expr = xpath.compile("/XML/User/@*");
+			NodeList nl = doc.getElementsByTagName("User");
+			for (int i = 0; i < nl.getLength(); i++) {
+				NamedNodeMap children = nl.item(i).getAttributes();
+				if(children.getNamedItem("fn").getNodeValue().equals(usr.getFn()) && children.getNamedItem("ln").getNodeValue().equals(usr.getLn())) {
+					Element newElement1 = doc.createElement("User");
+					newElement1.setAttribute("fn", usr.getFn());
+					newElement1.setAttribute("ln", usr.getLn());
+					newElement1.setAttribute("pw", encrypt.generateSecurePassword(usr.getPw(), salt));
+					newElement1.setAttribute("darkThem", "" + usr.getDarkTheme());
+					newElement1.setAttribute("fbToken", usr.getFbToken());
+					newElement1.setAttribute("twToken", usr.getTwToken());
+					newElement1.setAttribute("emUsr", usr.getEmUsr());
+					newElement1.setAttribute("emPwd", encrypt.generateSecurePassword(usr.getEmPwd(), salt));
+					Node n = doc.getDocumentElement();
+					n.replaceChild(newElement1, nl.item(i));
+				}
+				Transformer transformer = TransformerFactory.newInstance().newTransformer();
+				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+				StreamResult result = new StreamResult(new FileOutputStream("src\\resources\\files\\credentials.xml"));
+				DOMSource source = new DOMSource(doc);
+				transformer.transform(source, result);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setFbToken(String fbToken) {
+		this.fbToken = fbToken;
+	}
+
+	public void setTwToken(String twToken) {
+		this.twToken = twToken;
+	}
+
+	public void setEmUsr(String emUsr) {
+		this.emUsr = emUsr;
+	}
+
+	public void setEmPwd(String emPwd) {
+		this.emPwd = emPwd;
+	}
+
+	public void setDarkTheme(String darkTheme) {
+		this.darkTheme = darkTheme;
+	}
+
+
+	public static void main(String[] args) {
+		User gay = new User("Leo", "Nardo", "as", null, null, null, "0", "0");
+		gay.saveNewUser(gay);
+		gay.setDarkTheme("1");
+		gay.updateUsrInfo(gay);
+		//System.out.println(gay.isUsrRegistered(gay));
+	}
 }
